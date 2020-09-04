09b687526ead0e92186eb72c703d2fd3a055fe07
Meha <ahem4progs@gmail.com>
2020-09-04 21:58:32 +0530

    Event Bubbling preventDefault() and some CSS

---
+++ index.js
@@ -0,0 +1,24 @@
+function grandparentDivClicked() {
+    alert('grandparentDivClicked');
+}
+function parentDivClicked() {
+    alert('parentDivClicked');
+}
+function childDivClicked() {
+    alert('childDivClicked');
+}
+function anchorTagClicked(event,thisElement) {
+    event.target;
+    alert('anchorTagClicked');
+    event.preventDefault();
+    // event.stopPropagation();
+    // event.stopImmediatePropagation();
+}
+$(document).ready(function(){
+    $('a').on('click',(event)=>{
+        event.stopImmediatePropagation();
+        alert('jQuery Invoked');
+    }).on('click', ()=>{
+        alert('Hii');
+    })
+})
